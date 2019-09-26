72f4cd4316e3a7c2bfd7af5e2aa26a8f81ebaca2
selambeyu <melkam3392beyu@gmail.com>
2019-09-26 13:42:35 -0400

    creating service

--- server/service/user.js
+++ server/service/user.js
@@ -1,2 +1,39 @@
+(function () {
+
 var mongoose=require('mongoose');
 var user =mongoose.model('User');
+
+exports.createUser=function(data,callback){
+    user.create(data).then((response)=>{
+        callback(null,response);
+    },(error)=>{
+        callback(error,null);
+    });
+}
+
+exports.findUser=function(query,callback){
+user.findOne(query,callback);
+}
+
+exports.updateUserById=function(id,data,callback){
+    user.findOneAndUpdate({
+        _id:id
+    },data,(err,response)=>{
+        callback(err,response);
+    });
+}
+
+exports.updateUser=function(query,data,options,callback){
+    user.findByIdAndUpdate(query,data,options,(err,response)=>{
+        callback(err,response);
+    });
+}
+
+
+exports.deleteUser=function(query,callback){
+    user.deleteOne(query,callback);
+
+}
+
+
+})()
