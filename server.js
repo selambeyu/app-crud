22812209ffaa4e3e7b9b4154d55383533701555f
selambeyu <melkam3392beyu@gmail.com>
2019-11-02 04:05:06 -0400

    updated

--- server.js
+++ server.js
@@ -29,9 +29,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/',(req,res)=>{
+  res.send("heroku deployed succesfully!!")
+})
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/user',userRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
